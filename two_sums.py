30cc49cd4ae726194c601cfd427b28a303338869
Tanvir-Chowdhury <tanvirvlogger@gmail.com>
2024-08-07 20:38:28 +0600

    first commit

--- two_sums.py
+++ two_sums.py
@@ -1,1 +1,13 @@
-git
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict_num = {}
+        for i, num in enumerate(nums):
+            dict_num[num] = i
+        for i, num in enumerate(nums):
+            complement = target - int(num)
+            if complement in dict_num.keys() and dict_num[complement] != i:
+                result = list()
+                result.append(i)
+                result.append(dict_num[complement])
+                return result
+        return []
